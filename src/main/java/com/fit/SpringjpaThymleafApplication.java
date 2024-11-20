3195d900c85a09279c92428f8547a16cfb9760fb
QuocBao2003 <votranquocbao2806@gmail.com>
2024-11-20 08:25:38 +0700

    init commit

---
+++ src/main/java/com/fit/SpringjpaThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjpaThymleafApplication.class, args);
+    }
+
+}
